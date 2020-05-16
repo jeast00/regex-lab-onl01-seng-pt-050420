ee5ebbc985aff0e34fbbf6b4051383d3e86e2f8b
Jonathan East <jonathan_east@ymail.com>
2020-05-16 18:59:51 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,9 +1,11 @@
+require 'pry'
+
 def starts_with_a_vowel?(word)
   word.match(/\A[aeiou]/i) ? true : false
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  return text.grep(/\w+un&&w+ing/)
+
 end
 
 def words_five_letters_long(text)
